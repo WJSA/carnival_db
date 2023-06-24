d2393192cba00075443973448b5a0c3d00ccf150
William Serrano <william.serrano@estelio.com>
2023-06-24 10:54:35 -0400

    EscuelaList

--- client/src/componentes/EscuelaList.js
+++ client/src/componentes/EscuelaList.js
@@ -1,11 +1,14 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { Card, CardContent, Typography } from '@mui/material'
 
 export default function EscuelaList() {
+
+    const  [escuelas, setEscuelas] = useState([])
 
     const loadSchools = async () => {
         const response = await fetch('http://localhost:4000/escuelas')
         const data = await response.json()
-        console.log(data)
+        setEscuelas(data)
     }
 
     useEffect(() => {
@@ -15,6 +18,18 @@ export default function EscuelaList() {
     return (
         <>
             <h1>Lista de escuelas</h1>
+            {
+                escuelas.map((escuela) => (
+                    <Card>
+                        <CardContent>
+                            <Typography>{escuela.idescuela}</Typography>
+                            <Typography>{escuela.nombre}</Typography>
+                            <Typography>{escuela.fechafundacion}</Typography>
+                            <Typography>{escuela.direccion}</Typography>
+                        </CardContent>
+                    </Card>
+                ))
+            }
         </>
     )
 }
